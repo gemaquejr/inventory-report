59c5b13b3eff9b7e062e6e3975a94f14c0886136
gemaquejr <gemaquejr@hotmail.com>
2023-01-15 20:35:47 -0300

    Adiciona teste do construtor/inicializador do objeto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    mockProduct = Product(
+        123,
+        "newspaper",
+        "gemaque's Diary",
+        "15/01/2023",
+        "16/01/2023",
+        123321,
+        "seco e conservado",
+    )
+
+    assert mockProduct.id == 123
+    assert mockProduct.nome_da_empresa == "gemaque's Diary"
+    assert mockProduct.nome_do_produto == "newspaper"
+    assert mockProduct.data_de_fabricacao == "15/01/2023"
+    assert mockProduct.data_de_validade == "16/01/2023"
+    assert mockProduct.numero_de_serie == 123321
+    assert mockProduct.instrucoes_de_armazenamento == "seco e conservado"
