12aefd096a58f7369ec0a34d48c1d4864757b023
gemaquejr <gemaquejr@hotmail.com>
2023-01-16 03:56:34 -0300

    Adiciona relatorio .json

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -13,11 +14,18 @@ class Inventory:
                 data.append(row)
         return data
 
+    def import_json(path):
+        with open(path) as file:
+            path_json = json.load(file)
+            return path_json
+
     @classmethod
     def import_data(cls, path, type):
         data = []
         if path.endswith(".csv"):
             data = Inventory.import_csv(path)
+        if path.endswith(".json"):
+            data = Inventory.import_json(path)
 
         if type == "simples":
             return SimpleReport.generate(data)
