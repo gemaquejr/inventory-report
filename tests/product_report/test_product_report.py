ac685fca82003707b3457f7c38782491a7dc529f
gemaquejr <gemaquejr@hotmail.com>
2023-01-16 12:29:22 -0300

    Adiciona teste do relatorio individual

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    mockProduct = Product(
+        123,
+        "newspaper",
+        "gemaque's Diary",
+        "15/01/2023",
+        "16/01/2023",
+        123321,
+        "seco e conservado",
+    )
+
+    assert repr(mockProduct) == (
+        f"O produto {mockProduct.nome_do_produto}"
+        f" fabricado em {mockProduct.data_de_fabricacao}"
+        f" por {mockProduct.nome_da_empresa}"
+        f" com validade até {mockProduct.data_de_validade}"
+        f" precisa ser armazenado {mockProduct.instrucoes_de_armazenamento}."
+    )
